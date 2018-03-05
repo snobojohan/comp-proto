56928f4a6c505a1c0d879ba9885bd24cec71416b
Johan Hallberg <johan.hallberg@gmail.com>
2018-03-05 11:44:12 +0100

    before rob

--- src/components/Horiz.js
+++ src/components/Horiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Question from '../components/Question';
 import QuestionCount from '../components/QuestionCount';
@@ -15,9 +15,23 @@ import MdClose from 'react-icons/lib/md/close';
 import MdArrowDown from 'react-icons/lib/md/keyboard-arrow-down';
 // import {MdClose} from 'react-icons/md';
 
-function Horiz(props) {
+class Horiz extends Component {
+// function Horiz(props) {
 
-  function renderAnswerOptions(key) {
+  // var _self = this;
+
+  constructor () {
+      super()
+      this.state = {
+        isHidden: true
+      }
+
+      this.renderAnswerOptions = this.renderAnswerOptions.bind(this);
+    }
+
+  renderAnswerOptions (key,pp) {
+
+    console.log(pp);
 
     return (
       <div className="likert__option">
@@ -25,11 +39,14 @@ function Horiz(props) {
           type="radio"
           className="likert__radio"
           name="likertGroup"
-          checked={key.type === props.answer}
+
+          checked={key.type === this.props.answer}
           id={key.type}
           value={key.type}
-          disabled={props.answer}
-          onChange={props.onAnswerSelected}
+
+          disabled={this.props.answer}
+          onChange={this.props.onAnswerSelected}
+
         />
         <label className="likert__label" htmlFor={key.type}>
           <span className="likert__label__text" dangerouslySetInnerHTML={{__html: key.content}} />
@@ -38,151 +55,156 @@ function Horiz(props) {
     );
   }
 
-  return (
+  render () {
+    return (
 
 
 
 
-    <div className="layout-main">
+      <div className="card-main card-main--overflow-x layout-main">
 
 
-    <div className="layout-main-child--one sublayout-head">
-      <div className="head-bwd">
-        <a className="bwd-area" href="#">&larr;</a>
-      </div>
-      <div className="head-label">
-        <span className="label-area">ekonomi</span>
-      </div>
-      <div className="head-count">
-        <span className="counter-area">3 av 15 frågor</span>
-      </div>
+      <div className="layout-main-child--one sublayout-head">
+        <div className="head-bwd">
+          <a className="bwd-area" href="#">&larr;</a>
+        </div>
+        <div className="head-label">
+          <span className="label-area">ekonomi</span>
+        </div>
+        <div className="head-count">
+          <span className="counter-area">3 av 15 frågor</span>
+        </div>
 
-      <div className="sublayout-head__progress">
-        <div className="progbar">
-          <div className="progbar__inner">
+        <div className="sublayout-head__progress">
+          <div className="progbar">
+            <div className="progbar__inner">
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="layout-main-child--two">
+      <div className="layout-main-child--two">
 
-      <ReactCSSTransitionGroup
+        <ReactCSSTransitionGroup
 
-        className="container"
+          className="container"
 
-        component="div"
+          component="div"
 
-        transitionName="woho"
+          transitionName="woho"
 
-        transitionEnterTimeout={1000}
+          transitionEnterTimeout={1000}
 
-        transitionLeaveTimeout={1000}
+          transitionLeaveTimeout={1000}
 
-      >
+        >
 
-      <div className="container__child" key={props.questionId + '-cc'}>
+        <div className="container__child" key={this.props.questionId + '-cc'}>
 
-        <div className="sublayout-main" key={props.questionId}>
+          <div className="sublayout-main" key={this.props.questionId}>
 
-          <div className="sublayout-main-child--one sublayout-main__upper">
+            <div className="sublayout-main-child--one sublayout-main__upper">
 
-            <h2 className="question">{props.question}</h2>
+              <h2 className="typo-question">{this.props.question}</h2>
+
+            </div>
+
+            <div className="sublayout-main-child--two sublayout-main__jumper">
+
+              <a href="#" className="temp">Vad tycker partierna?</a>
+
+              <div className="parties">
+
+                <div className="parties__row">
+                  <a href="#">Vad tycker partierna?</a>
+
+                  <div className="parties__row__interact">
+                    <MdClose />
+                  </div>
+                </div>
+                <div className="parties__row">
+                  <div className="parties__row__figure">
+                    <img src={emojVb} alt="Väldigt missnöjd emoji" />
+                  </div>
+                  <h2>Mycket dåligt förslag</h2>
+                </div>
+
+                <div className="parties__row">
+                  <div className="parties__row__figure party party-l">
+                    L
+                  </div>
+                  <span className="party-longname">Liberalerna</span>
+                  <p>"Vi tycker att det är självklart att Sverige..."</p>
+                  <div className="parties__row__interact">
+                    <MdArrowDown />
+                  </div>
+                </div>
+
+                <div className="parties__row">
+                  <div className="parties__row__figure party party-m">
+                    <span>M</span>
+                  </div>
+                  <span className="party-longname">Moderaterna</span>
+                </div>
+
+                <div className="parties__row">
+                  <div className="parties__row__figure">
+                    <img src={emojPb} alt="Rätt så missnöjd emoji" />
+                  </div>
+                  <h2>Ganska dåligt förslag</h2>
+                </div>
+
+                <div className="parties__row">
+                  <div className="parties__row__figure party party-kd">
+                    <span>KD</span>
+                  </div>
+                  <span className="party-longname">Kristdemokraterna</span>
+                </div>
+
+                <div className="parties__row">
+                  <div className="parties__row__figure party party-c">
+                    <span>C</span>
+                  </div>
+                  <span className="party-longname">Centern</span>
+                </div>
+
+
+
+              </div>
+            </div>
+
+
+
+            <div className="sublayout-main-child--three">
+
+              <div className="extra">
+                <span className="important-area">
+                  <input type="checkbox" id="superImportant" name="superImportant" value="newsletter" />
+                  <label for="superImportant">Extra viktig fråga</label>
+                </span>
+              </div>
+
+
+              <div className="likert">
+
+                {this.props.answerOptions.map(this.renderAnswerOptions)}
+
+              </div>
+
+              <div className="footer-skip">
+                  <a href="#">Hoppa över frågan </a>
+              </div>
+            </div>
 
           </div>
 
-          <div className="sublayout-main-child--two sublayout-main__jumper">
-            <div className="parties">
 
-              <div className="parties__row">
-                <a href="#">Vad tycker partierna?</a>
-
-                <div className="parties__row__interact">
-                  <MdClose />
-                </div>
-              </div>
-              <div className="parties__row">
-                <div className="parties__row__figure">
-                  <img src={emojVb} alt="Väldigt missnöjd emoji" />
-                </div>
-                <h2>Mycket dåligt förslag</h2>
-              </div>
-
-              <div className="parties__row">
-                <div className="parties__row__figure party party-l">
-                  L
-                </div>
-                <span className="party-longname">Liberalerna</span>
-                <p>"Vi tycker att det är självklart att Sverige..."</p>
-                <div className="parties__row__interact">
-                  <MdArrowDown />
-                </div>
-              </div>
-
-              <div className="parties__row">
-                <div className="parties__row__figure party party-m">
-                  <span>M</span>
-                </div>
-                <span className="party-longname">Moderaterna</span>
-              </div>
-
-              <div className="parties__row">
-                <div className="parties__row__figure">
-                  <img src={emojPb} alt="Rätt så missnöjd emoji" />
-                </div>
-                <h2>Ganska dåligt förslag</h2>
-              </div>
-
-              <div className="parties__row">
-                <div className="parties__row__figure party party-kd">
-                  <span>KD</span>
-                </div>
-                <span className="party-longname">Kristdemokraterna</span>
-              </div>
-
-              <div className="parties__row">
-                <div className="parties__row__figure party party-c">
-                  <span>C</span>
-                </div>
-                <span className="party-longname">Centern</span>
-              </div>
-
-
-
-            </div>
           </div>
-
-
-
-          <div className="sublayout-main-child--three">
-
-            <div className="extra">
-              <span className="important-area">
-                <input type="checkbox" id="superImportant" name="superImportant" value="newsletter" />
-                <label for="superImportant">Extra viktig fråga</label>
-              </span>
-            </div>
-
-
-            <div className="likert">
-
-              {props.answerOptions.map(renderAnswerOptions)}
-
-            </div>
-
-            <div className="footer-skip">
-                <a href="#">Hoppa över frågan </a>
-            </div>
-          </div>
-
+        </ReactCSSTransitionGroup>
         </div>
-
-
-        </div>
-      </ReactCSSTransitionGroup>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 Horiz.propTypes = {
--- src/components/Info.js
+++ src/components/Info.js
@@ -8,53 +8,62 @@ import bars from '../graph-results.png';
 function Info(props) {
 
   return (
-    <ReactCSSTransitionGroup
-      className="container result"
-      component="div"
-      transitionName="fade"
-      transitionEnterTimeout={800}
-      transitionLeaveTimeout={500}
-      transitionAppear
-      transitionAppearTimeout={500}
-    >
-      <div className="partInfo">
+    <div className="card-main layout-main">
 
-        <h1>Testa vilka partier du matchar bäst i Stockholms kommunalval</h1>
-        <p>Söndagen den 9 september 2018 är det dags att välja. Se vilka partiers åsikter som matchar dig bäst i det kommunala valet.</p>
+      <div className="layout-main-child--one">
+        <h1 className="typo-page-head"><span className="typo-svt-slash">/</span>Valkompassen</h1>
       </div>
 
-        <div className="makeMeSticky">
-            <a href="/?regular=true" className="btnStart">STARTA KOMPASSEN<span>Det tar 5 - 10 minuter</span></a>
-        </div>
+      <div className="layout-main-child--two">
+        <ReactCSSTransitionGroup
+          className="container result"
+          component="div"
+          transitionName="fade"
+          transitionEnterTimeout={800}
+          transitionLeaveTimeout={500}
+          transitionAppear
+          transitionAppearTimeout={500}
+        >
+          <div className="partInfo">
 
-        <div className="partInfo">
+            <h1 className="typo-h1">Testa vilka partier du matchar bäst i Stockholms kommunalval</h1>
+            <p>Söndagen den 9 september 2018 är det dags att välja. Se vilka partiers åsikter som matchar dig bäst i det kommunala valet.</p>
+          </div>
 
+            <div className="makeMeSticky">
+                <a href="/?regular=true" className="btnStart">STARTA KOMPASSEN<span>Det tar 5 - 10 minuter</span></a>
+            </div>
 
-          <h3>Så styrs Stockholm nu</h3>
-
-          <img src={graph} alt="Mandat i sthlm" className="crisp halfImage" />
-          <p><strong>Socialdemokraterna</strong>, <strong>Miljöpartiet</strong>, <strong>Vänsterpartiet</strong> och <strong>Feministiskt initiativ</strong> är de partier som bildar majoritetsstyret med sammanlagt 53%.</p>
-
-
-          <h3>Valresultat kommunalval Stockholm 2014</h3>
-          <img src={bars} alt="Resultat" className="crisp halfImage" />
-          <p><strong>Moderaterna</strong> blev största parti med 28%.</p>
-
-        </div>
-
-
-        <div className="partInfo">
-        <h3>Så styrs en kommun</h3>
-
-        <p>Staten bestämmer vad kommunen skall göra men inte hur det skall ske. Kommunen måste ansvara för barnomsorgen, skolan, äldreomsorgen, räddningstjänsten, socialtjänsten, osv.</p>
-        <p>Men exakt hur den gör, hur stor kommunalskatten ska vara och hur pengarna ska fördelas är upp till varje kommun.</p>
+            <div className="partInfo">
 
 
-        </div>
+              <h3>Så styrs Stockholm nu</h3>
+
+              <img src={graph} alt="Mandat i sthlm" className="crisp halfImage" />
+              <p><strong>Socialdemokraterna</strong>, <strong>Miljöpartiet</strong>, <strong>Vänsterpartiet</strong> och <strong>Feministiskt initiativ</strong> är de partier som bildar majoritetsstyret med sammanlagt 53%.</p>
+
+
+              <h3>Valresultat kommunalval Stockholm 2014</h3>
+              <img src={bars} alt="Resultat" className="crisp halfImage" />
+              <p><strong>Moderaterna</strong> blev största parti med 28%.</p>
+
+            </div>
+
+
+            <div className="partInfo">
+            <h3>Så styrs en kommun</h3>
+
+            <p>Staten bestämmer vad kommunen skall göra men inte hur det skall ske. Kommunen måste ansvara för barnomsorgen, skolan, äldreomsorgen, räddningstjänsten, socialtjänsten, osv.</p>
+            <p>Men exakt hur den gör, hur stor kommunalskatten ska vara och hur pengarna ska fördelas är upp till varje kommun.</p>
+
+
+            </div>
 
 
 
-    </ReactCSSTransitionGroup>
+        </ReactCSSTransitionGroup>
+      </div>
+    </div>
   );
 
 }
